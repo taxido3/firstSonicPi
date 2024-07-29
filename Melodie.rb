27d58bb512831496218ad601a8ffc3c40313b2cb
Tarik Khairalla <t.khairalla@posteo.de>
2024-07-29 02:05:05 +0200

    vol as var

--- Melodie.rb
+++ Melodie.rb
@@ -1,49 +1,50 @@
 use_bpm 138
-
+bassVol = 0.3
+keyVol = 2
+#use_synth :dsaw
 live_loop :melodie do
   live_loop :bass do
-    play :A1
+    play :A1, amp: bassVol
     sleep 0.5
-    play :A1
+    play :A1, amp: bassVol
     sleep 0.5
-    play :A1
+    play :A1, amp: bassVol
     sleep 0.5
-    play :E2
+    play :E2, amp: bassVol
     sleep 1
-    play :E2
+    play :E2, amp: bassVol
     sleep 1
-    play :A2
+    play :A2, amp: bassVol
     sleep 0.5
   end
+  use_synth :growl
   live_loop :keys do
-    counter = 1
-    play :A5
+    play :A5, amp: keyVol
     sleep 0.5
-    play :A5
+    play :A5, amp: keyVol
     sleep 0.5
-    play :A5
+    play :A5, amp: keyVol
     sleep 0.5
-    play :E6
+    play :E6, amp: keyVol
     sleep 1
-    play :E6
+    play :E6, amp: keyVol
     sleep 1
-    play :F6
+    play :F6, amp: keyVol
     sleep 1
-    play :F6
+    play :F6, amp: keyVol
     sleep 0.5
-    play :F6
+    play :F6, amp: keyVol
     sleep 0.5
-    play :A6
+    play :A6, amp: keyVol
     sleep 1
-    play :F6
+    play :F6, amp: keyVol
     sleep 1
-    play :E6
+    play :E6, amp: keyVol
     sleep 1
-    play :A5
+    play :A5, amp: keyVol
     sleep 7
-    play :A5
+    play :A5, amp: keyVol
     sleep 0.5
-    counter = counter + 1
   end
   sleep 1
 end
